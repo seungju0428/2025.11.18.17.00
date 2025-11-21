f89c30b1d79985af711ed5d8efae4bc11e01df14
minsookim1674 <minsuzz60616@gmail.com>
2025-11-21 23:07:30 +0900

    Update Transaction.h

--- 2025.11.18.17.00/Transaction.h
+++ 2025.11.18.17.00/Transaction.h
@@ -1,14 +1,29 @@
-#ifndef	TRANSACTION_H
+#ifndef TRANSACTION_H
 #define TRANSACTION_H
-#include "Interface.h"
+
+#include <string>
+#include "ATM.h" // CashDenominations 구조체 사용
+
+using namespace std;
+
+class Session; // 전방 선언
 
 class Transaction {
-private:
 protected:
-	Interface& ui;
+    Session* pSession;      // child class들이 사용할 Session pointer
+    int transactionID;      // 고유 거래 ID
+    static int nextID;      // ID 생성을 위한 정적 변수
+
 public:
-	Transaction(Interface& uiInput);
-	virtual void run() = 0;
+    Transaction(Session* session);
+    virtual ~Transaction() {}
+
+    virtual void run() = 0;
+
+    // 공통 기능
+    int getTransactionID() const;
+    long calculateFee(const string& transactionType, const string& destBankName = "") const;
+    bool collectFee(long fee, CashDenominations& outFeeCash);
 };
 
 #endif
